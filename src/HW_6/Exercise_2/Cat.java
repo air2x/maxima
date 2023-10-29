b6848b8482de650ec3e3e0d573b3fa71bcd880d0
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-10-29 14:22:22 +0300

    'fixed HW_6 ex_2'

--- src/HW_6/Exercise_2/Cat.java
+++ src/HW_6/Exercise_2/Cat.java
@@ -8,32 +8,72 @@ public class Cat {
     String color;
 
     public Cat(String name) {
-        this.name = name;
+        setName(name);
     }
 
     public Cat(String name, int weight, int age) {
-        this.name = name;
-        this.weight = weight;
-        this.age = age;
+        this(name);
+        setWeight(weight);
+        setAge(age);
     }
 
     public Cat(String name, int age) {
-        this.name = name;
-        this.age = age;
+        this(name);
+        setAge(age);
     }
 
     public Cat(int weight, String color) {
-        this.weight = weight;
-        this.color = color;
+        setWeight(weight);
+        setColor(color);
     }
 
     public Cat(String address, String color, int weight) {
+        this(weight, color);
+        setAddress(address);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
         this.color = color;
     }
 
     public String toString() {
-        return "Name: " + name + ", Color: " + color + ", Address: " + address + ", Weight: " + weight + ", Age: " + age;
+        return "Name: " + getName() + ", Color: " + getColor() + ", Address: " + getAddress() + ", Weight: " +
+                    getWeight() + ", Age: " + getAge();
     }
 }
