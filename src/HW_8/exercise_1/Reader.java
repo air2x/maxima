7eb0d8ddde311546286000eff7d44c8bc9a0a697
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-11-02 22:26:02 +0300

    'add HW_8 ex2'

--- src/HW_8/exercise_1/Reader.java
+++ src/HW_8/exercise_1/Reader.java
@@ -6,4 +6,48 @@ public class Reader {
     private String department;
     private String dateOfBirth;
     private String phoneNum;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getCardNum() {
+        return cardNum;
+    }
+
+    public void setCardNum(int cardNum) {
+        this.cardNum = cardNum;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public void takeBook() {
+
+    }
 }
--- src/HW_8/exercise_2/Aspirant.java
+++ src/HW_8/exercise_2/Aspirant.java
@@ -1,4 +1,11 @@
 package HW_8.exercise_2;
 
-public class Aspirant {
+public class Aspirant extends Student {
+
+    private String scientificWorkName;
+
+    @Override
+    public int getScholarship() {
+        return getAverageMark() == 5 ? 2500 : 2200;
+    }
 }
