8f6ddc54e47f7b8da73c4069d4ae63d33f89a069
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-11-22 21:19:22 +0300

    'add hw_12'

---
+++ src/HW_12/Main.java
@@ -0,0 +1,23 @@
+package HW_12;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Fridge fridge = new Fridge();
+
+        fridge.addProduct("Хлеб", 2);
+        fridge.addProduct("Молоко", 2);
+        fridge.addProduct("Огурец", 8);
+        fridge.addProduct("Помидор", 20);
+
+        fridge.printFridge();
+
+        fridge.take("Огурец", 2);
+        fridge.printFridge();
+
+        fridge.addOrders("Молоко", 2);
+        fridge.printFridge();
+        fridge.addOrders("Кефир", 1);
+        fridge.printFridge();
+
+    }
+}
