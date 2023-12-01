00712a5c9b0507c5a8324b4a14e666df1481f6e0
Airat <airat91091@mail.ru>
2023-12-01 10:21:17 +0300

    'add EX_4 in HW_14'

--- src/HW_14/EX_4.java
+++ src/HW_14/EX_4.java
@@ -1,4 +1,9 @@
 package HW_14;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /*
 Написать программу, которая вводит с клавиатуры строку текста.
 Программа должна вывести на экран две строки:
@@ -31,4 +36,33 @@ public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э
     }
  */
 public class EX_4 {
+    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+    public static void main(String[] args) throws Exception {
+        List<Character> vowelLetters = new ArrayList<>();
+        List<Character> consonantlLetters = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку");
+        String str = sc.nextLine();
+        char[] strChar = str.toCharArray();
+        for (char c : strChar) {
+            if (c == 32) {
+                continue;
+            }
+            boolean b = isVowel(c) ? vowelLetters.add(c) : consonantlLetters.add(c);
+        }
+        vowelLetters.forEach(c -> System.out.print(c + " "));
+        System.out.println();
+        consonantlLetters.forEach(c -> System.out.print(c + " "));
+    }
+
+    public static boolean isVowel(char character) {
+        character = Character.toLowerCase(character);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+        for (char vowel : vowels) {  // ищем среди массива гласных
+            if (character == vowel) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
