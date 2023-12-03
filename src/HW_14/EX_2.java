fc92e94e328571a8106f90bd09ba09529ea20d27
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-12-03 22:31:31 +0300

    'add ex_2 hw_14'

--- src/HW_14/EX_2.java
+++ src/HW_14/EX_2.java
@@ -1,4 +1,9 @@
 package HW_14;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /*
 Программа вводит с клавиатуры пару (число и строку) и выводит их на экран.
 Нужно сделать так, чтобы программа вводила с клавиатуры пары (число и строку) и сохраняла их в HashMap.
@@ -25,4 +30,17 @@ package HW_14;
 1 Мама
  */
 public class EX_2 {
+    public static void main(String[] args) throws InterruptedException {
+        Map<String, Integer> map = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите пару число и строку в формате \"1 Мама\"");
+            String str = sc.nextLine();
+            if (str.isEmpty()) {
+                break;
+            }
+            map.put(str.split(" ")[1], Integer.parseInt(str.split(" ")[0]));
+        }
+        map.forEach((k, v) -> System.out.println(v + " " + k));
+    }
 }
