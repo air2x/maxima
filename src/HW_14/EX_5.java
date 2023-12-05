0a55a440c84e84279734ad078455368c1b70a6f9
Айрат_Шарафиев <sarafievajrat347@gmail.com>
2023-12-05 18:20:55 +0300

    'add ex_5 hw_14'

--- src/HW_14/EX_5.java
+++ src/HW_14/EX_5.java
@@ -1,4 +1,10 @@
 package HW_14;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 /*
 Задача: Пользователь вводит с клавиатуры список слов (и чисел).
 Слова вывести в возрастающем порядке, числа - в убывающем.
@@ -24,4 +30,49 @@ package HW_14;
 Яблоко
  */
 public class EX_5 {
+    public static void main(String[] args) {
+        List<String> input = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 8; i++) {
+            input.add(sc.nextLine());
+        }
+
+        List<Integer> nums = new ArrayList<>();
+        List<String> words = new ArrayList<>();
+        for (String word : input) {
+            if (isNumber(word)) {
+                nums.add(Integer.valueOf(word));
+            } else {
+                words.add(word);
+            }
+        }
+        Collections.sort(nums);
+        Collections.reverse(nums);
+        Collections.sort(words);
+
+        int wordI = 0;
+        int numI = 0;
+
+        for (String word : input) {
+            if (isNumber(word)) {
+                result.add(nums.get(numI).toString());
+                numI++;
+            } else {
+                result.add(words.get(wordI));
+                wordI++;
+            }
+        }
+        result.forEach(System.out::println);
+
+    }
+
+    private static boolean isNumber(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            if (!Character.isDigit(word.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
