a6db043d89f139bacdb5e989d706e8be84806e17
Airat <airat91091@mail.ru>
2023-12-05 07:06:28 +0300

    'fixed ex_3 hw_14'

--- src/HW_14/EX_3.java
+++ src/HW_14/EX_3.java
@@ -23,9 +23,32 @@ public class EX_3 {
             list.add(sc.nextInt());
         }
         System.out.println(list);
-        Collections.sort(list);
-//        list.sort(Comparator.comparingInt(a -> a));
+//        Collections.sort(list);
+        quickSort(list, 0, list.size() - 1);
         System.out.println(list);
         System.out.println(list.get(9) + "\n" + list.get(10));
+    }
+
+    //Алгоритм быстрой сортировки (Quick Sort или сортировка Хоара)
+    public static void quickSort(List<Integer> sortArr, int low, int high) {
+        if (sortArr.isEmpty() || low >= high) return;
+
+        int middle = low + (high - low) / 2;
+        int border = sortArr.get(middle);
+
+        int i = low, j = high;
+        while (i <= j) {
+            while (sortArr.get(i) < border) i++;
+            while (sortArr.get(j) > border) j--;
+            if (i <= j) {
+                int swap = sortArr.get(i);
+                sortArr.set(i, sortArr.get(j));
+                sortArr.set(j, swap);
+                i++;
+                j--;
+            }
+        }
+        if (low < j) quickSort(sortArr, low, j);
+        if (high > i) quickSort(sortArr, i, high);
     }
 }
